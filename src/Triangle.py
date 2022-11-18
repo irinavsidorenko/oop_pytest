341f5b6e02ba2ad2c9820238cb4f7f94d610f222
Irina S <koshkatonia@gmail.com>
2022-11-18 11:08:37 +0300

    add condition in  triangle class and add additional tests for this condition

--- src/Triangle.py
+++ src/Triangle.py
@@ -4,14 +4,23 @@ from src.Figure import Figure
 
 
 class Triangle(Figure):
+    """Override Triangle class constructor and add condition for triangle creation"""
+    def __init__(self, name, a, b, c):
+        super().__init__(name)
+        self.a = a
+        self.b = b
+        self.c = c
+        if self.a > self.b + self.c or self.b > self.a + self.c or self.c > self.a + self.b:
+            raise ValueError("Треугольник с такими сторонами создать невозможно!")
+
     """ Calculate triangle perimeter """
     @property
     def perimeter(self):
-        return self.side[0] + self.side[1] + self.side[2]
+        return self.a + self.b + self.c
 
     """ Calculate triangle area """
     @property
     def area(self):
         half_perimeter = self.perimeter / 2
-        return math.sqrt(half_perimeter * (half_perimeter - self.side[0]) * (half_perimeter - self.side[1]) * (
-                    half_perimeter - self.side[2]))
+        return math.sqrt(half_perimeter * (half_perimeter - self.a) * (half_perimeter - self.b) * (
+                    half_perimeter - self.c))
--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -6,19 +6,34 @@ import pytest
 name = "Trinity"
 
 def test_triangle_perimeter():
-    triangle = Triangle(name, 1, 2, 3)
+    triangle = Triangle(name, a=1, b=2, c=3)
     assert triangle.perimeter == 6
 
 def test_triangle_area():
-    triangle = Triangle(name, 7, 7, 3)
+    triangle = Triangle(name, a=7, b=7, c=3)
     assert triangle.area == 10.256095748383007
 
 def test_triangle_add_area():
-    triangle_first = Triangle(name, 2, 2, 3)
-    triangle_second = Triangle(name, 2, 2, 3)
+    triangle_first = Triangle(name, a=2, b=2, c=3)
+    triangle_second = Triangle(name, a=2, b=2, c=3)
     assert triangle_first.add_area(triangle_second) == 3.968626966596886
 
 def test_triangle_add_area_exception():
-    triangle = Triangle(name, 1, 2, 3)
+    triangle = Triangle(name, a=1, b=2, c=3)
     with pytest.raises(ValueError):
         triangle.add_area(0)
+
+@pytest.mark.parametrize("value", [-1, 0, 33])
+def test_triangle_create_exception_side_a(value):
+    with pytest.raises(ValueError):
+        Triangle(name, a=value, b=1, c=2)
+
+@pytest.mark.parametrize("value", [-1, 0, 33])
+def test_triangle_create_exception_side_b(value):
+    with pytest.raises(ValueError):
+        Triangle(name, a=1, b=value, c=2)
+
+@pytest.mark.parametrize("value", [-1, 0, 33])
+def test_triangle_create_exception_side_c(value):
+    with pytest.raises(ValueError):
+        Triangle(name, a=1, b=2, c=value)
